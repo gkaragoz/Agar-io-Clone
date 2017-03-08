3062256decba2850071bb8a975e232d56694af95
gkaragoz <annotto3@gmail.com>
2017-03-09 00:34:35 +0300

    Add eat script and implement to the project.

---
+++ Client/Assets/Scripts/Eat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : MonoBehaviour {
+
+    public string Tag;
+    public float Increase;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == Tag)
+        {
+            transform.localScale += new Vector3(Increase, Increase, Increase);
+            Destroy(other.gameObject);
+        }
+    }
+}
