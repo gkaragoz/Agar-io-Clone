9f374b469caf2264f89d8e8c01c6ff1a7a4570b6
gkaragoz <annotto3@gmail.com>
2017-03-09 00:21:34 +0300

    Add random colors script.

---
+++ Client/Assets/Scripts/Color.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour {
+
+    public List<Material> Mats = new List<Material>();
+
+    void Awake()
+    {
+        GetComponent<Renderer>().material = Mats[Random.Range(0, Mats.Count)];
+    } 
+}
