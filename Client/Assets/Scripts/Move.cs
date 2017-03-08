2fd5646f2e82ff296bb3be0fe5cc0bdc26e3a4c2
gkaragoz <annotto3@gmail.com>
2017-03-09 00:13:17 +0300

    Add player move script.

---
+++ Client/Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float Speed;
+
+    void Update()
+    {
+        Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        target.z = transform.position.z;
+
+        transform.position = Vector3.MoveTowards(transform.position, target, Speed * Time.deltaTime / transform.localScale.x);
+    }
+}
