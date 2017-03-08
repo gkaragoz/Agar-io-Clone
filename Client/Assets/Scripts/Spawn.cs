9d6fb1ca3cce9ce86da6b095824f64dd360be5c6
gkaragoz <annotto3@gmail.com>
2017-03-09 00:28:17 +0300

    Add food generation script and implement it.

---
+++ Client/Assets/Scripts/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject Food;
+    public float Speed;
+
+    void Start()
+    {
+        InvokeRepeating("Generate", 0, Speed);
+    }
+
+    void Generate()
+    {
+        int x = Random.Range(0, Camera.main.pixelWidth);
+        int y = Random.Range(0, Camera.main.pixelHeight);
+
+        Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0));
+        target.z = 0;
+
+        Instantiate(Food, target, Quaternion.identity);
+    }
+}
